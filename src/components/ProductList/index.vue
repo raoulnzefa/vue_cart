58688a9e312af1bc0fdbf80dfe7ad2e24a882576
stanislavskiy <stanislavskiy.sanya@gmail.com>
2018-07-29 12:06:52 +0300

    Refactoring

---
+++ src/components/ProductList/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul
+    id="productList"
+    class="product-list"
+    v-if="items && items.length>0"  
+  >
+    <li 
+      class="product-list__item"
+      v-for="item in items"
+      :key="item.id"
+    >
+      <h3 class="product-list__item-title">
+        {{item.name}}
+      </h3>
+      <img :src="item.picture" alt="img">
+      <h4>{{item.price}}</h4>
+    </li>
+    </ul>  
+</template>
+
+<script>
+export default {
+  name: "ProductList",
+  props: {
+    items: {
+      type: Array,
+      default: null
+    }
+  }
+};
+</script>
+
--- src/components/pages/PageProducts/index.vue
+++ src/components/pages/PageProducts/index.vue
@@ -1,30 +1,19 @@
 <template>
   <div id="pageProducts" class="page-products">
     <h2>Products</h2>
-    <ul 
-      class="page-products__list"
-      v-if="productList && productList.length>0"  
-    >
-      <li 
-        class="page-products__item"
-        v-for="product in productList"
-        :key="product.id"
-      >
-        <h3 class="page-products__item-title">
-          {{product.name}}
-        </h3>
-        <img :src="product.picture" alt="img">
-        <h4>{{product.price}}</h4>
-      </li>
-    </ul>  
+    <product-list :items="productList" />
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import ProductList from "../../ProductList";
 
 export default {
   name: "PageProducts",
+  components: {
+    ProductList
+  },
   computed: {
     ...mapGetters(["productList"])
   }
