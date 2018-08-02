92a2a9318c9999269104a5e696ffc2d3bcaf91e7
stanislavskiy <stanislavskiy.sanya@gmail.com>
2018-08-02 11:29:13 +0300

    testing

---
+++ test/unit/specs/productMutations.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import { mutations } from '../../../src/store/index';
+
+const { updateProductList } = mutations
+
+describe('Vuex module: product.mutations', () => {
+  it('should update a list of products', () => {
+    const state = {
+      productList: null
+    }
+    const list = [1, 2, 3]
+
+    updateProductList(state, list)
+
+    expect(state.productList).to.equal(list)
+  })
+})
