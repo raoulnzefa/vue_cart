dca85d28bf075aaf471dcdf94f64c9c3c39756bd
stanislavskiy <stanislavskiy.sanya@gmail.com>
2018-08-07 15:55:02 +0300

    New component

---
+++ src/components/ProductAmountSetter/index.vue
@@ -0,0 +1,74 @@
+<template>
+  <div
+    v-if="item"
+    id="productAmountSetter"
+    class="product-amount-setter row text-center mt-2 mb-4"
+  >
+    <div class="col-12 d-flex justify-content-between">
+      <!-- Decrease amount -->
+      <a
+        class="btn d-flex align-items-center"
+        @click="decreaseOrderValue(item)"
+      >
+        <icon name="minus" scale="1"></icon>
+      </a>
+      <input
+        class="form-control mx-3 text-center"
+        type="number"
+        v-model="itemNumber"
+        min="1"
+        @focusout="validateInput"
+      >
+      <!-- Increase amount -->
+      <a
+        class="btn d-flex align-items-center"
+        @click="increaseOrderValue(item)"
+      >
+        <icon name="plus" scale="1"></icon>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+import Icon from 'vue-awesome/components/Icon'
+
+import 'vue-awesome/icons/plus'
+import 'vue-awesome/icons/minus'
+
+export default {
+  name: 'ProductAmountSetter',
+  components: {
+    Icon
+  },
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    /* Two-way computed property for
+    changing the value in the input */
+    itemNumber: {
+      get: function () {
+        return this.item.number
+      },
+      set: function (value) {
+        if (value > 0) this.setOrderValue({ product: this.item, value: value })
+      }
+    }
+  },
+  methods: {
+    ...mapActions([
+      'setOrderValue',
+      'increaseOrderValue',
+      'decreaseOrderValue'
+    ]),
+    validateInput (e) {
+      if (parseInt(e.target.value) < 1) e.target.value = 1
+    }
+  }
+}
+</script>
---
+++ test/unit/specs/component.ProductAmountSetter.spec.js
@@ -0,0 +1,84 @@
+import { mount, createLocalVue } from 'vue-test-utils'
+import Vuex from 'vuex'
+import ProductAmountSetter from '../../../src/components/ProductAmountSetter'
+import { Product } from '../../testutils/classes'
+
+const localVue = createLocalVue()
+
+localVue.use(Vuex)
+
+describe('Component: ProductAmountSetter', () => {
+  let actions
+  let store
+  let wrapper
+  let item
+
+  beforeEach(() => {
+    actions = {
+      setOrderValue: sinon.spy()
+    }
+    store = new Vuex.Store({
+      actions
+    })
+
+    item = new Product(1, 'Product', 50)
+    item.date = new Date()
+    item.number = 1
+
+    wrapper = mount(ProductAmountSetter, {
+      store,
+      localVue,
+      propsData: {
+        item
+      }
+    })
+  })
+
+  describe('Computed', () => {
+    describe('itemNumber', () => {
+      it(`should return the number of the current item`, () => {
+        expect(wrapper.vm.itemNumber).to.equal(item.number)
+      })
+
+      it(`should dispatch the "setOrderValue" action when assigning a value`, () => {
+        const newValue = 10
+        wrapper.vm.itemNumber = newValue
+
+        return actions.setOrderValue.should.have.been.called
+      })
+
+      it(`should not to dispatch the "setOrderValue" action when assigning 
+          a value that is less than 1`, () => {
+        const newValue = 0
+        wrapper.vm.itemNumber = newValue
+
+        return actions.setOrderValue.should.not.have.been.called
+      })
+    })
+  })
+
+  describe('Methods', () => {
+    describe('validateInput', () => {
+      const event = {
+        target: {
+          value: 0
+        }
+      }
+
+      it(`should set the value of the input to "1" if the current value is 
+          less than 1`, () => {
+        wrapper.vm.validateInput(event)
+
+        expect(event.target.value).to.equal(1)
+      })
+
+      it(`should not to set the value of the input to "1" if the current value
+          is not less than 1`, () => {
+        event.target.value = 10
+        wrapper.vm.validateInput(event)
+
+        expect(event.target.value).to.equal(10)
+      })
+    })
+  })
+})
