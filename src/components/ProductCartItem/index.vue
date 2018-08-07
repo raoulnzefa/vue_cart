f278f3af56e4299f030f38fe8811adb7de9a1d9f
stanislavskiy <stanislavskiy.sanya@gmail.com>
2018-08-07 15:58:00 +0300

    Component update

--- src/components/ProductCartItem/index.vue
+++ src/components/ProductCartItem/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     id="ProductCartItem"
-    class="product-cart-item col-lg-3 mt-5"
+    class="product-cart-item col-lg-3 col-sm-12 col-md-4 mt-5"
   >
     <product
           :item="item"
@@ -17,29 +17,7 @@
           </h6>
         </div>
       </div>
-      <div class="row text-center mt-2 mb-4">
-        <div class="col-12 d-flex justify-content-between">
-          <a
-            class="btn d-flex align-items-center"
-            @click="decreaseOrderValue(item)"
-          >
-            <icon name="minus" scale="1"></icon>
-          </a>
-          <input
-            class="product-cart-item__number-input form-control mx-3 text-center"
-            type="number"
-            v-model="itemNumber"
-            min="1"
-            @focusout="validateInput"
-          >
-          <a
-            class="btn d-flex align-items-center"
-            @click="increaseOrderValue(item)"
-          >
-            <icon name="plus" scale="1"></icon>
-          </a>
-        </div>
-      </div>
+      <product-amount-setter :item="item" />
     </product>
   </div>
 </template>
@@ -47,16 +25,13 @@
 <script>
 import { mapActions, mapGetters } from 'vuex'
 import Product from '../Product'
-import Icon from 'vue-awesome/components/Icon'
-
-import 'vue-awesome/icons/plus'
-import 'vue-awesome/icons/minus'
+import ProductAmountSetter from '../ProductAmountSetter'
 
 export default {
   name: 'ProductCartItem',
   components: {
     Product,
-    Icon
+    ProductAmountSetter
   },
   props: {
     item: {
@@ -65,28 +40,14 @@ export default {
     }
   },
   computed: {
-    ...mapGetters(['currency']),
+    ...mapGetters(['currency'])
     /* Two-way computed property for
     changing the value in the input */
-    itemNumber: {
-      get: function () {
-        return this.item.number
-      },
-      set: function (value) {
-        if (value > 0) this.setOrderValue({ product: this.item, value: value })
-      }
-    }
   },
   methods: {
     ...mapActions([
-      'setOrderValue',
-      'increaseOrderValue',
-      'decreaseOrderValue',
       'removeFromCart'
-    ]),
-    validateInput (e) {
-      if (parseInt(e.target.value) < 1) e.target.value = 1
-    }
+    ])
   }
 }
 </script>
