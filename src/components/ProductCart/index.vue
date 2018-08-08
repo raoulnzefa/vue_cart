8bcc84a46b9893e37fc5a8d89bce1ffc8afe42a3
stanislavskiy <stanislavskiy.sanya@gmail.com>
2018-08-08 06:49:37 +0300

    Refactor

--- src/components/ProductCart/index.vue
+++ src/components/ProductCart/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="productCart" class=" container-fluid product-cart">
+  <div id="productCart" class="container-fluid product-cart">
     <!-- List of selected products -->
     <div
       class="product-cart__list row"
@@ -14,39 +14,26 @@
     <div v-else class="row">
       <div class="col text-center mt-5">
         <span class="h4 text-black-50">
-          Cart is empty
+          The cart is empty
         </span>
       </div>
     </div>
-    <div
+    <product-cart-bottom-nav
       v-if="selectedProductsCount>0"
-      class="navbar fixed-bottom navbar-expand-lg navbar-light bg-light"
-    >
-      <div class="col d-flex justify-content-between my-2">
-        <!-- Total price -->
-        <h4 class="text-muted">
-          Total price: {{selectedProductsPrice}}{{currency}}
-        </h4>
-        <!-- Clear cart -->
-        <button
-          class="btn btn-outline-danger"
-          @click="clearCart()"
-        >
-          Clear cart
-        </button>
-      </div>
-    </div>
+    />
   </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
 import ProductCartItem from '../ProductCartItem'
+import ProductCartBottomNav from '../ProductCartBottomNav'
 
 export default {
   name: 'ProductCart',
   components: {
-    ProductCartItem
+    ProductCartItem,
+    ProductCartBottomNav
   },
   computed: {
     ...mapGetters([
