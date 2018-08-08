16e08b87b48d44ff48e8fa252e3b5f73f7dac81b
stanislavskiy <stanislavskiy.sanya@gmail.com>
2018-08-08 07:08:01 +0300

    New component

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,25 @@
 <template>
   <div id="app" class="app">
     <header>
-      <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-        <div class="col d-flex justify-content-start align-items-end py-3">
-          <router-link
-            class="app__link navbar-brand py-0 mr-3 text-dark"
-            :to="{name:'products'}"
-            tag="a"
-          >
-            <span class="h1 app__title">
-              Store
-            </span>
-          </router-link>
-          <icon name="chevron-right" class=" mr-3 mb-2"/>
-          <span class="h5 text-capitalize text-muted mb-1">
-            {{$route.name}}
-          </span>
-        </div>
-        <div class="col d-flex justify-content-end">
-          <router-link
-            class="app__link"
-            :to="{name:'cart'}"
-            tag="a"
-          >
-            <icon name="shopping-cart" scale=2></icon>
-            <span v-if="selectedProductsCount>0" class="badge badge-danger">
-              {{ selectedProductsCount }}
-            </span>
-          </router-link>
-        </div>
-      </nav>
+      <app-navbar />
     </header>
     <router-view class="app__content" />
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import Icon from 'vue-awesome/components/Icon'
-
-import 'vue-awesome/icons/shopping-cart'
-import 'vue-awesome/icons/chevron-right'
+import AppNavbar from './components/AppNavbar'
 
 export default {
   name: 'App',
   components: {
-    Icon
+    AppNavbar
   },
   created () {
     // Load product list
     this.$store.dispatch('loadProductList')
     // Load cart from local storage
     this.$store.dispatch('loadCart')
-  },
-  computed: {
-    ...mapGetters(['selectedProductsCount'])
   }
 }
 </script>
---
+++ src/components/AppNavbar/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+    <div class="col d-flex justify-content-start align-items-end py-3">
+      <router-link
+        class="app__link navbar-brand py-0 mr-3 text-dark"
+        :to="{name:'products'}"
+        tag="a"
+      >
+        <span class="h1 app__title">
+          Store
+        </span>
+      </router-link>
+      <icon name="chevron-right" class=" mr-3 mb-2"/>
+      <span class="h5 text-capitalize text-muted mb-1">
+        {{$route.name}}
+      </span>
+    </div>
+    <div class="col d-flex justify-content-end">
+      <router-link
+        class="app__link"
+        :to="{name:'cart'}"
+        tag="a"
+      >
+        <icon name="shopping-cart" scale=2></icon>
+        <span v-if="selectedProductsCount>0" class="badge badge-danger">
+          {{ selectedProductsCount }}
+        </span>
+      </router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import Icon from 'vue-awesome/components/Icon'
+
+import 'vue-awesome/icons/shopping-cart'
+import 'vue-awesome/icons/chevron-right'
+
+export default {
+  name: 'AppNavbar',
+  components: {
+    Icon
+  },
+  computed: {
+    ...mapGetters(['selectedProductsCount'])
+  }
+}
+</script>
