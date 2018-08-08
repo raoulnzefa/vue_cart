8ecb6d5e95178053121ce9a132f6fa036d7af280
stanislavskiy <stanislavskiy.sanya@gmail.com>
2018-08-08 06:49:18 +0300

    New component

---
+++ src/components/ProductCartBottomNav/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+      class="navbar fixed-bottom navbar-expand-lg navbar-light bg-light"
+    >
+      <div class="col d-flex justify-content-between my-2">
+        <!-- Total price -->
+        <h4 class="text-muted">
+          Total price: {{selectedProductsPrice}}{{currency}}
+        </h4>
+        <!-- Clear cart -->
+        <button
+          class="btn btn-outline-danger"
+          @click="clearCart()"
+        >
+          Clear cart
+        </button>
+      </div>
+    </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  name: 'ProductCartBottomNav',
+
+  computed: {
+    ...mapGetters([
+      'selectedProductsPrice',
+      'currency'
+    ])
+  },
+  methods: {
+    ...mapActions(['clearCart'])
+  }
+}
+</script>
